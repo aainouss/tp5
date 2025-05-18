b9cb46d605f67dc31174651d3eeaa853c578b25d
said <you@example.com>
2025-05-18 11:13:13 +0200

    tp5 commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+
+tab : string[] = ['said','amine','lilia','celia'];
+
+supprimer(){
+  this.tab.pop();
+}
+
+vider(){
+  this.tab=[];
+}
+
+ajouter(nom:string){
+  this.tab.push(nom);
+}
+
+}
